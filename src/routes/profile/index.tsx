9f8bc757dfbde54b4dc6ecc7a4dd7f05bd745ce6
Farman Shaik <shkfrmn786@gmail.com>
2025-12-29 11:35:16 +0530

    auction in profile

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -90,12 +90,12 @@ function CreateProfile() {
     getRafflePurchasedCards,
     getGumballCreatedCards,
     getGumballPurchasedCards,
-    getGumballFavouriteCards,
     getAuctionCreatedCards,
-    getAuctionFavouriteCards,
+    getAuctionPurchasedCards,
   } = useProfileStats(publicKey?.toBase58() ?? "");
 
-  const {getFavouriteRaffle, getFavouriteGumball} = useQueryFavourites(publicKey?.toBase58() ?? "");
+  const { getFavouriteRaffle, getFavouriteGumball, getFavouriteAuction } =
+    useQueryFavourites(publicKey?.toBase58() ?? "");
   const raffleStats = getRaffleStats.data?.stats;
   const gumballStats = getGumballStats.data?.stats;
   const auctionStats = getAuctionStats.data?.stats;
@@ -104,10 +104,12 @@ function CreateProfile() {
   const gumballCreatedCards = getGumballCreatedCards.data ?? [];
   const gumballPurchasedCards = getGumballPurchasedCards.data ?? [];
   const auctionCreatedCards = getAuctionCreatedCards.data ?? [];
+  const auctionPurchasedCards = getAuctionPurchasedCards.data ?? [];
 
   const favouriteRaffles = getFavouriteRaffle.data;
   const favouriteGumballs = getFavouriteGumball.data;
-  console.log("favouriteGumballs",favouriteGumballs);
+  const favouriteAuctions = getFavouriteAuction.data;
+
   return (
     <main className="main font-inter">
       <section className="w-full pt-[60px] pb-[120px]">
@@ -116,28 +118,29 @@ function CreateProfile() {
             <div className="flex-1 space-y-5 md:max-w-[320px]">
               <div className="w-full bg-gray-1300 border border-gray-1100 rounded-[18px] py-5">
                 <div className="w-full px-4">
-                  <div className={`w-full flex ${publicKey ? "justify-between" : "justify-center"} items-center  gap-5 mb-4`}>
-                    {publicKey? (
+                  <div
+                    className={`w-full flex ${publicKey ? "justify-between" : "justify-center"} items-center  gap-5 mb-4`}
+                  >
+                    {publicKey ? (
                       <>
-                      <h4 className="text-lg text-primary-color font-inter font-semibold">
-                        {publicKey?.toBase58().slice(0, 6) +
-                          "....." +
-                          publicKey?.toBase58().slice(-6)}
-                      </h4>
-                      <div className="flex items-center gap-4">
-                      <a href="#">
-                        <img
-                          src="/icons/solana-sol-logo.svg"
-                          className="w-6 h-6"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                    </>
-                    ):(
+                        <h4 className="text-lg text-primary-color font-inter font-semibold">
+                          {publicKey?.toBase58().slice(0, 6) +
+                            "....." +
+                            publicKey?.toBase58().slice(-6)}
+                        </h4>
+                        <div className="flex items-center gap-4">
+                          <a href="#">
+                            <img
+                              src="/icons/solana-sol-logo.svg"
+                              className="w-6 h-6"
+                              alt=""
+                            />
+                          </a>
+                        </div>
+                      </>
+                    ) : (
                       <WalletMultiButton className=" w-full h-11 px-6 py-2.5 rounded-full bg-linear-to-r from-black-1000 via-neutral-500 to-black-1000 hover:from-primary-color hover:via-primary-color hover:to-primary-color text-white  font-semibold" />
                     )}
-                    
                   </div>
                 </div>
 
@@ -176,14 +179,12 @@ function CreateProfile() {
                       key={filter.value}
                       onClick={() => {
                         setRafflerFilter(
-                          filter.value as
-                            | "created"
-                            | "purchased"
-                            | "favourite"
+                          filter.value as "created" | "purchased" | "favourite"
                         );
-                       
-                          setActiveRafflerTab(filter.value as "created" | "purchased" | "favourite");
-                        
+
+                        setActiveRafflerTab(
+                          filter.value as "created" | "purchased" | "favourite"
+                        );
                       }}
                       className={`
                             text-sm cursor-pointer transition px-5 py-3 text-start font-semibold font-inter w-full rounded-full
@@ -238,7 +239,7 @@ function CreateProfile() {
                           Purchase Volume
                         </p>
                         <p className="md:text-base text-sm font-medium font-inter text-black-1000 text-right">
-                          {(raffleStats?.purchaseVolume / 10 ** 9) || ""}
+                          {raffleStats?.purchaseVolume / 10 ** 9 || ""}
                         </p>
                       </li>
                     </>
@@ -266,7 +267,7 @@ function CreateProfile() {
                           Gumball Volume
                         </p>
                         <p className="md:text-base text-sm font-medium font-inter text-black-1000 text-right">
-                          {gumballStats?.totalVolumeSpent/10**9}
+                          {gumballStats?.totalVolumeSpent / 10 ** 9}
                         </p>
                       </li>
                     </>
@@ -369,22 +370,26 @@ function CreateProfile() {
                         className={`grid ${
                           activeRafflerTab === `purchased`
                             ? `grid-cols-1`
-                            : activeRafflerTab === `favourite` ? `grid-cols-3` : `lg:grid-cols-3 md:grid-cols-2 grid-cols-1`
+                            : activeRafflerTab === `favourite`
+                              ? `grid-cols-3`
+                              : `lg:grid-cols-3 md:grid-cols-2 grid-cols-1`
                         } lg:gap-y-10 lg:gap-x-[26px] gap-4`}
                       >
-                        {activeRafflerTab === "purchased" ? (
-                          rafflePurchasedCards.map((card: any) => (
-                            <RafflersCardPurchased key={card.id} {...card} />
-                          ))
-                        ) : activeRafflerTab === "favourite" ? (
-                          favouriteRaffles?.map((card: any) => (
-                            <CryptoCard key={card.id} raffle={card} soldTickets={card.ticketSold} />
-                          ))
-                        ) : (
-                          raffleCreatedCards.map((card: any) => (
-                            <RafflersCard key={card.id} {...card} />
-                          ))
-                        )}
+                        {activeRafflerTab === "purchased"
+                          ? rafflePurchasedCards.map((card: any) => (
+                              <RafflersCardPurchased key={card.id} {...card} />
+                            ))
+                          : activeRafflerTab === "favourite"
+                            ? favouriteRaffles?.map((card: any) => (
+                                <CryptoCard
+                                  key={card.id}
+                                  raffle={card}
+                                  soldTickets={card.ticketSold}
+                                />
+                              ))
+                            : raffleCreatedCards.map((card: any) => (
+                                <RafflersCard key={card.id} {...card} />
+                              ))}
                       </div>
                     )}
                   </>
@@ -392,20 +397,57 @@ function CreateProfile() {
 
                 {mainFilter === "Auctions" && (
                   <>
-                    {getAuctionCreatedCards.isLoading  ? (
+                    {getAuctionCreatedCards.isLoading ? (
                       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-                        {Array.from({ length: 6 }).map((_, i) => <CryptoCardSkeleton key={i} />)}
+                        {Array.from({ length: 6 }).map((_, i) => (
+                          <CryptoCardSkeleton key={i} />
+                        ))}
                       </div>
                     ) : auctionCreatedCards.length < 1 ? (
                       <NoAuctions />
                     ) : (
-                      <div className="w-full grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-y-10 lg:gap-x-[26px] gap-4">
-                        {auctionCreatedCards.map(card => (
-                          <div key={card.auctionPda} className="flex items-center justify-center">
-                            {/* <AucationsCard key={card.auctionPda} {...card} /> */}
-                            <></>
-                          </div>
-                        ))}
+                      <div
+                        className={`grid ${
+                          activeRafflerTab === "purchased"
+                            ? "grid-cols-1"
+                            : "lg:grid-cols-3 md:grid-cols-2 grid-cols-1"
+                        } lg:gap-y-10 lg:gap-x-[26px] gap-4`}
+                      >
+                        {activeRafflerTab === "purchased"
+                          ? auctionPurchasedCards.map((r: any) => (
+                              <AuctionsCard
+                                key={r.id}
+                                {...r}
+                                id={r.id ?? 0}
+                                prizeName={r.prizeName ?? ""}
+                                prizeImage={r.prizeImage ?? ""}
+                                collectionName={r.collectionName ?? ""}
+                                reservePrice={r.reservePrice ?? ""}
+                              />
+                            ))
+                          : activeRafflerTab === "favourite"
+                            ? favouriteAuctions?.map((r: any) => (
+                                <AuctionsCard
+                                  key={r.id}
+                                  {...r}
+                                  id={r.id ?? 0}
+                                  prizeName={r.prizeName ?? ""}
+                                  prizeImage={r.prizeImage ?? ""}
+                                  collectionName={r.collectionName ?? ""}
+                                  reservePrice={r.reservePrice ?? ""}
+                                />
+                              ))
+                            : auctionCreatedCards.map((r: any) => (
+                                <AuctionsCard
+                                  key={r.id}
+                                  {...r}
+                                  id={r.id ?? 0}
+                                  prizeName={r.prizeName ?? ""}
+                                  prizeImage={r.prizeImage ?? ""}
+                                  collectionName={r.collectionName ?? ""}
+                                  reservePrice={r.reservePrice ?? ""}
+                                />
+                              ))}
                       </div>
                     )}
                   </>
@@ -413,7 +455,8 @@ function CreateProfile() {
 
                 {mainFilter === "Gumballs" && (
                   <>
-                    {getGumballCreatedCards.isLoading || getGumballPurchasedCards.isLoading ? (
+                    {getGumballCreatedCards.isLoading ||
+                    getGumballPurchasedCards.isLoading ? (
                       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
                         {Array.from({ length: 6 }).map((_, i) => (
                           <CryptoCardSkeleton key={i} />
@@ -432,20 +475,23 @@ function CreateProfile() {
                             : "lg:grid-cols-3 md:grid-cols-2 grid-cols-1"
                         } lg:gap-y-10 lg:gap-x-[26px] gap-4`}
                       >
-                        {activeRafflerTab === "purchased" ? (
-                          gumballPurchasedCards.map((card: any) => (
-                            <GumballsCardPurchased key={card.id} gumball={card} />
-                          ))
-                        ) : activeRafflerTab === "favourite" ? (
-                          favouriteGumballs?.map((card: any) => (
-                            <GumballsCard key={card.id} gumball={card} />
-                          ))
-                        ) : (
-                          gumballCreatedCards.map((card: any) => (
-                            <GumballsCardCreated key={card.id} gumball={card} />
-                          ))
-                        )}
-                          
+                        {activeRafflerTab === "purchased"
+                          ? gumballPurchasedCards.map((card: any) => (
+                              <GumballsCardPurchased
+                                key={card.id}
+                                gumball={card}
+                              />
+                            ))
+                          : activeRafflerTab === "favourite"
+                            ? favouriteGumballs?.map((card: any) => (
+                                <GumballsCard key={card.id} gumball={card} />
+                              ))
+                            : gumballCreatedCards.map((card: any) => (
+                                <GumballsCardCreated
+                                  key={card.id}
+                                  gumball={card}
+                                />
+                              ))}
                       </div>
                     )}
                   </>
