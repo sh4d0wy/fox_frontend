126c0616d2a2671a1e4d1be6daa75d7959dc7ff5
sh4d0wy <sakshambhugra8@gmail.com>
2026-01-07 18:49:47 +0530

    chore: testing without api url

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_URL } from "../src/constants";
 
 export const api = axios.create({
-    baseURL: `${API_URL}/api`,
+    baseURL: `/api`,
     headers: {
         "Content-Type": "application/json",
         'ngrok-skip-browser-warning': 'true',
