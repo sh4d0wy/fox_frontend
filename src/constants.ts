fef3d784300754682534ad6a919e600accfd1231
Farman Shaik <shkfrmn786@gmail.com>
2026-01-14 15:40:29 +0530

    change rpc

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const HELIUS_KEY = "0fcc653b-d9ed-4c30-8ad8-6bda0b48da85";
+export const HELIUS_KEY = import.meta.env.VITE_HELIUS_API_KEY;
 // change to "mainnet" when deploying to mainnet
 export const NETWORK = "mainnet" as "devnet" | "mainnet";
-export const SOLANA_RPC = (NETWORK === "devnet") ? "https://api.devnet.solana.com" : "https://solana-mainnet.g.alchemy.com/v2/DEsRBMlNZIATyz66p1-Vh";
+export const SOLANA_RPC = (NETWORK === "devnet") ? "https://api.devnet.solana.com" : `https://mainnet.helius-rpc.com/?api-key=${HELIUS_KEY}`;
 export const API_URL = "";
 // export const API_URL = "http://3.39.230.132";
