a5808bb32b476cc72f71fa4e1481dcff1da2a52d
Farman Shaik <shkfrmn786@gmail.com>
2026-01-24 18:57:55 +0530

    fix err

--- src/components/gumballs/GumballSetup.tsx
+++ src/components/gumballs/GumballSetup.tsx
@@ -220,7 +220,7 @@ export const GumballSetup = () => {
                                 ticketPrice: parseFloat(ticketPrice) || 0,
                                 isTicketSol:ticketCurrency.address===NATIVE_SOL_MINT,
                                 startGumball: startType === "manual",
-                                ticketMint: new PublicKey(ticketCurrency.address ? ticketCurrency.address : WRAPPED_SOL_MINT),
+                                ticketMint: ticketCurrency.address === NATIVE_SOL_MINT ? new PublicKey(WRAPPED_SOL_MINT) : new PublicKey(ticketCurrency.address),
                               });
                             } catch (error) {
                               toast.error("Failed to create gumball");
